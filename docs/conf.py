58f1d931d403b5d9860fe399dd5a7115e9934086
robertmichael0215 <robertmichael0215@outlook.com>
2026-02-03 15:16:04 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,9 @@ author = 'geek-squad-appointment'
 release = '1.0'
 
 # Extensions
-extensions = [
-    'sphinx_sitemap',
-]
+extensions = ['sphinx_sitemap']
 
-# Paths
+# Templates
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -15,14 +13,14 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# Custom JS & Favicon
-html_js_files = ['chatbot.js']  # Make sure _static/chatbot.js exists
-html_favicon = '_static/favicon.png'  # Make sure _static/favicon.png exists
+# JS & Favicon
+html_js_files = ['chatbot.js']
+html_favicon = '_static/favicon.png'
 
-# Bing verification (optional)
+# Bing verification code
 html_context = {
     'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
 }
 
-# Base URL for sitemap
+# Sitemap base URL
 html_baseurl = 'https://geeksquadguide.readthedocs.io/en/latest/'
